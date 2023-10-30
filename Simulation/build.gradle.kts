d0a1a7006103678a1d668ddbd34e06c1843e19a0
safroalex <alexsafr.a@ya.ru>
2023-10-30 17:11:13 +0300

    add javafx to graddle

--- Simulation/build.gradle.kts
+++ Simulation/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("java")
     id("application")
+    id("org.openjfx.javafxplugin") version "0.0.9"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 application {
@@ -14,11 +20,14 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+javafx {
+    version = "17"
+    modules("javafx.controls", "javafx.fxml")
+}
 
+dependencies {
+    implementation(platform("org.junit:junit-bom:5.9.1"))
+    implementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
--- Simulation/src/main/java/org/safroalex/Main.java
+++ Simulation/src/main/java/org/safroalex/Main.java
@@ -1,10 +1,28 @@
 package org.safroalex;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class Main extends Application {
 
-public class Main {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Click me");
+        btn.setOnAction(event -> System.out.println("Ohh yes!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Simple test app");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        System.out.println("Jackson library is working!");
+        launch(args);
     }
 }
